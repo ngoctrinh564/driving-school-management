14eaefc36eea10217fc62d400d8352ca8910a9d4
haiit <leduyhai090704@gmail.com>
2026-04-05 09:17:32 +0700

    update trangthai KHOAHOC tu dong khi chay project

---
+++ driving-school-management/Services/DbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace driving_school_management.Services
+{
+    public class DbInitializer
+    {
+        private readonly string _connectionString;
+
+        public DbInitializer(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("OracleDb");
+        }
+
+        public void Init()
+        {
+            using (var conn = new OracleConnection(_connectionString))
+            {
+                conn.Open();
+
+                using (var cmd = new OracleCommand("pkg_khoahoc.cap_nhat_trang_thai", conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
